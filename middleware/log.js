6e85507d3798ead6f095c45405b7ddf67b2fe5cd
Ada <wb-xyl390954@alibaba-inc.com>
2020-11-19 15:05:58 +0800

    feat:init

--- middleware/log.js
+++ middleware/log.js
@@ -1,21 +1,21 @@
-const { isTarget, isJSON } = require('./util')
+const { isTarget, isJSON } = require('../common/util');
 module.exports = async (ctx, next) => {
   const {
-    request: { body = {}, url, method, header },
-  } = ctx
-  const start = Date.now()
-  await next()
-  const end = Date.now()
+    request: { body = {}, url, method },
+  } = ctx;
+  const start = Date.now();
+  await next();
+  const end = Date.now();
   try {
-    const res = JSON.parse(isJSON(ctx.body, 'String') ? ctx.body : '{}')
+    const res = JSON.parse(isJSON(ctx.body, 'String') ? ctx.body : '{}');
     const log = `[${method} ${end - start}ms ${url}] 
 request: ${JSON.stringify(isTarget(body, 'Object') ? body : {})} 
 response: ${JSON.stringify(isTarget(res, 'Object') ? res : {})}
-    `
-    console.log(log)
+    `;
+    console.log(log);
   } catch (error) {
-    console.log('error', error)
+    console.log('error', error);
   }
 
   // console.log(body)
-}
+};
--- web/pc/src/component/Demo/index.js
+++ web/pc/src/component/Demo/index.js
@@ -1,13 +1,22 @@
+import { useState, useCallback } from 'react';
 import { testApi } from './service';
 import styles from './index.less';
 
 export default () => {
-  const test = async () => {
+  const [data, setData] = useState({ timer: 0 });
+  const test = useCallback(async () => {
     const result = await testApi();
-  };
+    if (result.success) {
+      setData({
+        ...result,
+        timer: data.timer + 1,
+      });
+    }
+  }, [data.timer]);
   return (
     <div className={styles.test}>
       <a onClick={test}>api test</a>
+      <div>{JSON.stringify(data, '', 10)}</div>
     </div>
   );
 };
