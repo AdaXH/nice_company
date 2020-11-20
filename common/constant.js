7679aa4c16a922284b6e2603d1fa41cd465a5ea9
Ada <wb-xyl390954@alibaba-inc.com>
2020-11-20 14:49:21 +0800

    chore:code

--- common/constant.js
+++ common/constant.js
@@ -1,3 +1,45 @@
-module.exports = {
+exports.FILTER_URL = {
   FILTER_URL: ['Baiduspider', 'Googlebot', 'favicon.ico'],
 };
+
+// 基础中间件
+exports.BASE_MIDDLEWARES = [
+  () =>
+    // bodyparser
+    require('koa-bodyparser')({
+      enableTypes: ['json', 'form', 'text'],
+      jsonLimit: '5mb',
+      formLimit: '5mb',
+      textLimit: '5mb',
+    }),
+  (app) => {
+    // session
+    app.keys = ['secret'];
+    const CONFIG = {
+      key: 'SESSION_ID' /** (string) cookie key (default is koa:sess) */,
+      maxAge: 172800000,
+      autoCommit: true /** (boolean) automatically commit headers (default true) */,
+      overwrite: true /** (boolean) can overwrite or not (default true) */,
+      httpOnly: false /** (boolean) httpOnly or not (default true) */,
+      signed: true /** (boolean) signed or not (default true) */,
+      rolling: false /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
+      renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
+    };
+    return require('koa-session')(CONFIG, app);
+  },
+  () => require('koa-json')(), // json
+  (_, { baseUrl }) =>
+    // static
+    require('koa-static')(baseUrl + '/public'),
+  (_, { baseUrl }) =>
+    // koa-views
+    require('koa-views')(baseUrl + '/views', {
+      extension: 'ejs',
+    }),
+  () => {
+    const registerRouter = require('./registerRouter');
+    const router = require('koa-router')();
+    registerRouter(router);
+    return [router.routes(), router.allowedMethods()];
+  },
+];
--- koa.js
+++ koa.js
@@ -1,71 +1,20 @@
 const koa = require('koa');
-const bodyparser = require('koa-bodyparser');
-const json = require('koa-json');
-const views = require('koa-views');
-const mongoose = require('mongoose');
-const session = require('koa-session');
-const router = require('koa-router')();
-const registerRouter = require('./common/registerRoutet');
-const useMiddleware = require('./common/useMiddleware');
-
 let app = new koa();
+const { getEnv, registerBaseMiddleware } = require('./common/util');
+const useMiddleware = require('./common/useMiddleware');
+const config = require('./serverConfig');
 
-// middlewar
 app = useMiddleware(app);
+app = registerBaseMiddleware(app, [{ baseUrl: __dirname }]);
 
-const config = {
-  ...require('./common/serverConfig'),
-  undefined: {
-    host: 'mongodb://localhost:27017/company',
-  },
-};
-
-app.use(
-  bodyparser({
-    enableTypes: ['json', 'form', 'text'],
-    jsonLimit: '50mb',
-    formLimit: '50mb',
-    textLimit: '50mb',
-  })
-);
-
-app.keys = ['secret'];
-const CONFIG = {
-  key: 'SESSION_ID',
-  maxAge: 172800000,
-  autoCommit: true,
-  overwrite: true,
-  httpOnly: false,
-  signed: true,
-  rolling: false,
-  renew: false,
-};
-app.use(session(CONFIG, app));
-
-app.use(json());
-app.use(require('koa-static')(__dirname + '/public'));
-
-app.use(
-  views(__dirname + '/views', {
-    extension: 'ejs',
-  })
-);
-
-//controllers
-registerRouter(router);
-//router
-app.use(router.routes(), router.allowedMethods());
-
-app.listen(config.port, (_) => console.log('server on ' + config.port));
-
-//error
 app.on('error', (err, ctx) => {
+  ctx.body = `error on server ${JSON.stringify(err)}`;
+  // todo: should send a error - page
   console.log('server error', err, ctx);
 });
 
-//connect to mongo
-// mongoose.connect(config[process.env.NODE_ENV].host).then((res) => {
-//   res
-//     ? console.log('connected to mongo')
-//     : console.log('can not connect to mongo')
-// })
+app.listen(config.port, (_) => {
+  console.log('server on %s', config.port);
+  // const database = new Database(config[getEnv()]);
+  // database.connect();
+});
